ee4ccc07b76135d03d98c1d8739f5c06f790e1b7
Tgithinji <t.muraguri420@gmail.com>
2024-03-15 02:38:12 +0300

    Use the List from the typing module to annotate lists

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 """
 5-sum_list.py
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Takes a list of floats as arguments and
     returns their sum
