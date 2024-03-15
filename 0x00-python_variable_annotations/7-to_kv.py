e196d73e172881e987d81051c8b0a3fff05ed000
Tgithinji <t.muraguri420@gmail.com>
2024-03-15 03:15:25 +0300

    Function that converts a string and int into a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+7-to_kv module
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    converts a string and int/float to a tuple
+
+    parameters:
+    k: str
+        a string
+    v: Union[int, float]
+         a float or string
+
+    Returns:
+        a tuple containing a string and a float
+    """
+    return (k, v)
