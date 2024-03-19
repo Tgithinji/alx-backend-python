00901a751c0f0d54459eb9cadf10e29c66ec060a
Tgithinji <t.muraguri420@gmail.com>
2024-03-20 00:27:17 +0300

    Alter wait n to a new function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+1-concurrent_coroutine module
+"""
+from typing import List
+import asyncio
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawns wait_random n times
+    """
+    delays_list = []
+    sorted_list = []
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        delays_list.append(task)
+
+    for task in asyncio.as_completed(delays_list):
+        delay = await task
+        sorted_list.append(delay)
+
+    return sorted_list
