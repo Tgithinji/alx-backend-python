ee1c3601cd664f69de7d9a26c877b47b812656be
Tgithinji <t.muraguri420@gmail.com>
2024-07-31 18:29:37 +0300

    Use the gather method

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,12 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measure the total runtime and return it"""
     start_time: float = time.perf_counter()
-    await asyncio.gather(
-       async_comprehension(),
-       async_comprehension(),
-       async_comprehension(),
-       async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time: float = time.perf_counter()
     runtime: float = end_time - start_time
     return runtime
